ad5d42e4b748dd056f1ec5b82db221706627e266
Kapil Chaudhari <chaudharikapil2708@gmail.com>
2026-07-26 23:54:42 +0530

    Add files via upload

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./navbar.css"
+import logo from "../assets/crewlink.png";
 export default function Navbar() {
+
     return (
 
         <>
             <nav className="navbar navbar-expand-lg py-3 sticky-top">
                 <div className="container">
-                    <a className="navbar-brand fw-bold fs-4" href="#"><img src="src\assets\crewlink.png" className="logo" />CrewLink</a>
+                    <a
+                        className="navbar-brand d-flex align-items-center fw-bold fs-4"
+                        href="#"
+                    >
+                        <img src={logo} alt="CrewLink Logo" className="logo" />
+                        <span>CrewLink</span>
+                    </a>                    
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -26,8 +34,8 @@ export default function Navbar() {
                             </li>
                         </ul>
                         <div className="d-flex gap-3">
-                            <button className="btn btn-outline-dark">Register</button>
-                            <button className="btn btn-dark">Login</button>
+                            <button className="btn btn-outline-dark">Login</button>
+                            <button className="btn btn-dark">Register</button>
                         </div>
                     </div>
                 </div>
