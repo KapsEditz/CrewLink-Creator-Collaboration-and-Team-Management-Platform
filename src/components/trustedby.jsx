6fdb9c122e61fddb5fd394f6886ac3f5fbf805cc
Kapil Chaudhari <chaudharikapil2708@gmail.com>
2026-07-31 22:34:01 +0530

    Add files via upload

---
+++ src/components/trustedby.jsx
@@ -0,0 +1,22 @@
+import "./trustedby.css";
+
+export default function TrustedBy() {
+    return (
+        <section className="trusted">
+
+            <p className="trusted-title">
+                Trusted by creators from
+            </p>
+
+            <div className="trusted-list">
+                <span>Creators</span>
+                <span>Editors</span>
+                <span>Designers</span>
+                <span>Developers</span>
+                <span>Founders</span>
+                <span>Agencies</span>
+            </div>
+
+        </section>
+    );
+}
